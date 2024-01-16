300e16674f5fc99bd6d54057d6169a5ca0c7d80a
tberry825 <tracief515@gmail.com>
2024-01-16 11:50:37 -0600

    adding error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ErrorBoundary from './ErrorBoundary.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ErrorBoundary fallback="Unfortunately, you have reached and error in this app, Please refresh your screen and try again.">
     <App />
+    </ErrorBoundary>
+    
   </React.StrictMode>,
 )
