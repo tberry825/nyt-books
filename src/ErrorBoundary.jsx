7daf5ee567d82af0b008ffcaa1b1fe4d225074e7
tberry825 <tracief515@gmail.com>
2024-01-16 12:34:10 -0600

    adding error boundary2

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
-    state = { hasError: false };
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+      }
     static getDerivedStateFromError(error) {
         return { hasError: true };
     }
